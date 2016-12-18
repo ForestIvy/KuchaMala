c22b836dbf0edc393b293f2d4af55de6e06d69d9
Irina Yatsenko <yveu@hotmail.com>
2016-12-18 11:52:34 -0800

    Min and Max possible intervals

--- VariousProblems/Spoj_AGGRCOW.cpp
+++ VariousProblems/Spoj_AGGRCOW.cpp
@@ -7,21 +7,52 @@
 #include <vector>
 #include <string>
 #include <functional>
-#include <unordered_set>
-#include <iostream>
+#include <algorithm>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
 namespace SPOJ
 {
-	TEST_CLASS(JobOffers)
+	class CAggrCow
+	{
+		vector<int> m_v;
+	public:
+		CAggrCow(vector<int> v)
+			: m_v(move(v))
+		{
+			if (m_v.size() < 2) throw;
+			sort(m_v.begin(), m_v.end());
+		}
+
+		int FindMinInterval()
+		{
+			int minInterval = FindMaxInterval();
+			for (auto i = 1; i < m_v.size(); i++)
+			{
+				auto cand = m_v[i] - m_v[i - 1];
+				if (cand < minInterval)
+					minInterval = cand;
+			}
+			return minInterval;
+		}
+
+		int FindMaxInterval()
+		{
+			return m_v.back() - m_v.front();
+		}
+	};
+
+	TEST_CLASS(AGGRCOW)
 	{
 	public:
 
-		TEST_METHOD(SelectJobOffers_OneOffer)
+		TEST_METHOD(FindMinAndMaxBoundaries)
 		{
-			Assert::Fail();
+			vector<int> v{4, 17, 10, 9, 6, 1};
+			CAggrCow ac(v);
+			Assert::AreEqual(1, ac.FindMinInterval());
+			Assert::AreEqual(16, ac.FindMaxInterval());
 		}
 	};
 }
