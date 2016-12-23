8b8201f4207bb552a94f5f2907f16c48987cafd9
Irina Yatsenko <yveu@hotmail.com>
2016-12-23 14:39:54 -0800

    Started working on ACODE problem from SPOJ

---
+++ VariousProblems/Spoj_ACODE.cpp
@@ -0,0 +1,25 @@
+// http://www.spoj.com/problems/ACODE/
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <algorithm>
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+#if 1
+namespace SPOJ
+{
+	TEST_CLASS(ACODE)
+	{
+	public:
+
+		TEST_METHOD(SingleDigit)
+		{
+			Assert::Fail();
+		}
+	};
+}
+#endif
