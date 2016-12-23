31c87214a06cf00fea6bf3fedafc172eac559703
Irina Yatsenko <yveu@hotmail.com>
2016-12-22 20:02:27 -0800

    Accepted by SPOJ

--- VariousProblems/Spoj_INVCNT.cpp
+++ VariousProblems/Spoj_INVCNT.cpp
@@ -9,7 +9,7 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
-#if 1
+#if 0
 namespace SPOJ
 {
 	long long CountInversions(vector<long>& v, size_t start, size_t end)
@@ -74,6 +74,37 @@ namespace SPOJ
 			if (v[i - 1] > v[i]) throw 0;
 
 		return to_string(res);
+	}
+
+	int main()
+	{
+		long tests = 0;
+		cin >> tests;
+
+		vector<long long> inversions;
+		inversions.reserve(tests);
+		for (long i = 0; i < tests; i++)
+		{
+			long n = 0;
+			cin >> n;
+			vector<long> v;
+			v.reserve(n);
+			for (long j = 0; j < n; j++)
+			{
+				long a;
+				cin >> a;
+				v.push_back(a);
+			}
+			inversions.push_back(CountInversions(v, 0, v.size() - 1));
+		}
+
+		cout << endl;
+		for (auto i : inversions)
+			cout << i << endl;
+
+		char quit;
+		std::cin >> quit;
+		return 0;
 	}
 
 	TEST_CLASS(INVCNT)
--- console/console.cpp
+++ console/console.cpp
@@ -8,14 +8,31 @@
 
 using namespace std;
 
-
 int main()
 {
-	vector<long> coins;
-	long c;
-	while (coins.size() < 10 && cin >> c)
-		coins.push_back(c);
-	
+	long tests = 0;
+	cin >> tests;
+
+	vector<long long> inversions;
+	inversions.reserve(tests);
+	for (long i = 0; i < tests; i++)
+	{
+		long n = 0;
+		cin >> n;
+		vector<long> v;
+		v.reserve(n);
+		for (long j = 0; j < n; j++)
+		{
+			long a;
+			cin >> a;
+			v.push_back(a);
+		}
+		//inversions.push_back(CountInversions(v, 0, v.size() - 1));
+	}
+
+	cout << endl;
+	for (auto i : inversions)
+		cout << i << endl;
 
 	char quit;
 	std::cin >> quit;
