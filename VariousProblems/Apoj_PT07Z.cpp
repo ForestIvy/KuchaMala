7a3d307b9f83cdf53ed2423b321020d7f2a69dca
Irina Yatsenko <yveu@hotmail.com>
2016-12-25 20:24:20 -0800

    Started working on the longest path in tree problems from SPOJ (PT07Z)

---
+++ VariousProblems/Apoj_PT07Z.cpp
@@ -0,0 +1,25 @@
+// http://www.spoj.com/problems/PT07Z/
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <limits>
+#include <vector>
+#include <algorithm>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+#if 1
+namespace SPOJ
+{
+	TEST_CLASS(PT07Z)
+	{
+	public:
+		TEST_METHOD(SingleVertex)
+		{
+			Assert::Fail();
+		}
+	};
+}
+#endif
