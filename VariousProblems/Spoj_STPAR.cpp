07f15a91495ee3b7a829175a8bd3f61111681972
Irina Yatsenko <yveu@hotmail.com>
2016-12-29 16:03:26 -0800

    Set up for STPAR problem from SPOJ

---
+++ VariousProblems/Spoj_STPAR.cpp
@@ -0,0 +1,27 @@
+// http://www.spoj.com/problems/STPAR/
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+#if 1
+namespace SPOJ
+{
+	TEST_CLASS(STPAR)
+	{
+	public:
+		TEST_METHOD(SingleCar)
+		{
+			Assert::Fail();
+		}
+
+	};
+}
+#endif
