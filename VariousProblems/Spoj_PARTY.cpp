9218190bfd6fcdd49a71d73cc4e049941a65971d
Irina Yatsenko <yveu@hotmail.com>
2016-12-31 11:48:44 -0800

    Set up for solving PARTY problem from SPOJ

---
+++ VariousProblems/Spoj_PARTY.cpp
@@ -0,0 +1,24 @@
+// http://www.spoj.com/problems/PARTY/
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <algorithm>
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+#if 1
+namespace SPOJ
+{
+	TEST_CLASS(PARTY)
+	{
+	public:
+		TEST_METHOD(SingleItemDoesNotFit)
+		{
+			Assert::Fail();
+		}
+	};
+}
+#endif
