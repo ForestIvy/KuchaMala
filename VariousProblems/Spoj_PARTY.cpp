abf0859a714b0c8ea122f8af88e33d7c1d214f80
Irina Yatsenko <yveu@hotmail.com>
2016-12-31 13:17:23 -0800

    Cleaned up after solving PARTY

--- VariousProblems/Spoj_PARTY.cpp
+++ VariousProblems/Spoj_PARTY.cpp
@@ -9,7 +9,7 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
-#if 1
+#if 0
 namespace SPOJ
 {
 	struct Item
@@ -45,6 +45,36 @@ namespace SPOJ
 		auto it = find_if_not(values_prev.rbegin(), values_prev.rend(), [&](int v) { return v == values_prev.back();});
 		return Item(values_prev.rend() - it, values_prev.back());
 	}
+
+#if 0
+	int main()
+	{
+		int capacity, citems;
+		cin >> capacity >> citems;
+		vector<Item> results;
+		while (capacity != 0 || citems != 0)
+		{
+			vector<Item> items;
+			items.reserve(citems);
+			for (int i = 0; i < citems; i++)
+			{
+				int s, v;
+				cin >> s >> v;
+				items.push_back(Item(s, v));
+			}
+			results.push_back(Fill(capacity, items));
+
+			cin >> capacity >> citems;
+		}
+
+		for (auto r : results)
+			cout << r.size << " " << r.value << endl;
+
+		char q;
+		cin >> q;
+		return 0;
+	}
+#endif
 
 	TEST_CLASS(PARTY)
 	{
--- console/console.cpp
+++ console/console.cpp
@@ -10,63 +10,8 @@
 
 using namespace std;
 
-struct Item
-{
-	int size = 0;
-	int value = 0;
-	Item(int s, int v) : size(s), value(v) {}
-};
-
-Item Fill(int capacity, const vector<Item>& items)
-{
-	vector<int> values_prev(capacity + 1, 0);
-
-	for (int i = 0; i < items.size(); i++)
-	{
-		vector<int> values(capacity + 1, 0);
-		for (int cur_cap = 1; cur_cap <= capacity; cur_cap++)
-		{
-			if (items[i].size > cur_cap)
-			{
-				values[cur_cap] = values_prev[cur_cap];
-			}
-			else
-			{
-				int v_take = items[i].value + values_prev[cur_cap - items[i].size];
-				int v_skip = values_prev[cur_cap];
-				values[cur_cap] = max(v_take, v_skip);
-			}
-		}
-		values_prev = values;
-	}
-
-	auto it = find_if_not(values_prev.rbegin(), values_prev.rend(), [&](int v) { return v == values_prev.back();});
-	return Item(values_prev.rend() - it, values_prev.back());
-}
-
 int main()
 {
-	int capacity, citems;
-	cin >> capacity >> citems;
-	vector<Item> results;
-	while (capacity != 0 || citems != 0)
-	{
-		vector<Item> items;
-		items.reserve(citems);
-		for (int i = 0; i < citems; i++)
-		{
-			int s, v;
-			cin >> s >> v;
-			items.push_back(Item(s, v));
-		}
-		results.push_back(Fill(capacity, items));
-
-		cin >> capacity >> citems;
-	}
-
-	for (auto r : results)
-		cout << r.size << " " << r.value << endl;
-
 	char q;
 	cin >> q;
 	return 0;
