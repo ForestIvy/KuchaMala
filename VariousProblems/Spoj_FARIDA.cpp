b3f894f7500b2e267d46501fd6b65af6772f7237
Irina Yatsenko <yveu@hotmail.com>
2016-12-31 18:04:17 -0800

    Set up for FARIDA problem from SPOJ

---
+++ VariousProblems/Spoj_FARIDA.cpp
@@ -0,0 +1,24 @@
+// http://www.spoj.com/problems/FARIDA
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <algorithm>
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+#if 1
+namespace SPOJ
+{
+	TEST_CLASS(FARIDA)
+	{
+	public:
+		TEST_METHOD(SingleMonster)
+		{
+			Assert::Fail();
+		}
+	};
+}
+#endif
