2e2ec6699a22b124b6aeaa7575f5c4681c2154cf
Irina Yatsenko <yveu@hotmail.com>
2016-12-31 19:08:14 -0800

    FARIDA accepted by SPOJ

--- VariousProblems/Spoj_FARIDA.cpp
+++ VariousProblems/Spoj_FARIDA.cpp
@@ -12,12 +12,75 @@ using namespace std;
 #if 1
 namespace SPOJ
 {
+	long long CollectCoins(const vector<long>& coins)
+	{
+		if (coins.empty()) return 0;
+		if (coins.size() == 1) return coins[0];
+		if (coins.size() == 2) return max(coins[0], coins[1]);
+
+		long long prev1 = max(coins[0], coins[1]);
+		long long prev2 = coins[0];
+		for (int i = 2; i < coins.size(); i++)
+		{
+			auto cur = max(coins[i] + prev2, prev1);
+			prev2 = prev1;
+			prev1 = cur;
+		}
+		return prev1;
+	}
+
+	struct CoinCollector
+	{
+		long long prev1 = 0;
+		long long prev2 = 0;
+
+		void Inspect(long coin)
+		{
+			auto cur = max(coin + prev2, prev1);
+			prev2 = prev1;
+			prev1 = cur;
+		}
+	};
+
 	TEST_CLASS(FARIDA)
 	{
+		string CollectCoins_(const vector<long>& coins)
+		{
+			CoinCollector cc;
+			for (auto c : coins) cc.Inspect(c);
+			return to_string(cc.prev1);
+		}
+
 	public:
 		TEST_METHOD(SingleMonster)
 		{
-			Assert::Fail();
+			Assert::AreEqual<string>("7", CollectCoins_(vector<long>{7}));
+
+
+		}
+
+		TEST_METHOD(TwoMonsters)
+		{
+			Assert::AreEqual<string>("7", CollectCoins_(vector<long>{7, 4}), L"7 4");
+			Assert::AreEqual<string>("7", CollectCoins_(vector<long>{4, 7}), L"4 7");
+		}
+
+		TEST_METHOD(ThreeMonsters)
+		{
+			Assert::AreEqual<string>(to_string(7 + 3), CollectCoins_(vector<long>{3, 4, 7}), L"3 4 7");
+			Assert::AreEqual<string>(to_string(12), CollectCoins_(vector<long>{3, 12, 7}), L"3 12 7");
+		}
+
+		TEST_METHOD(FourMonsters)
+		{
+			Assert::AreEqual<string>(to_string(3 + 7), CollectCoins_(vector<long>{3, 4, 7, 5}), L"3 4 7 5");
+			Assert::AreEqual<string>(to_string(6 + 5), CollectCoins_(vector<long>{3, 6, 7, 5}), L"3 6 7 5");
+			Assert::AreEqual<string>(to_string(7 + 5), CollectCoins_(vector<long>{7, 5, 4, 5}), L"7 5 4 5");
+		}
+
+		TEST_METHOD(MaxMonsters)
+		{
+			Assert::AreEqual<string>("5000000000000", CollectCoins_(vector<long>(10000,1000000000)));
 		}
 	};
 }
--- console/console.cpp
+++ console/console.cpp
@@ -10,8 +10,44 @@
 
 using namespace std;
 
+struct CoinCollector
+{
+	long long prev1 = 0;
+	long long prev2 = 0;
+
+	void Inspect(long coin)
+	{
+		auto cur = max(coin + prev2, prev1);
+		prev2 = prev1;
+		prev1 = cur;
+	}
+};
+
+
 int main()
 {
+	int tests;
+	cin >> tests;
+	vector<long long> results;
+
+	while (results.size() < tests)
+	{
+		int ncoins;
+		cin >> ncoins;
+
+		CoinCollector cc;
+		for (int i = 0; i < ncoins; i++)
+		{
+			long coin;
+			cin >> coin;
+			cc.Inspect(coin);
+		}
+		results.push_back(cc.prev1);
+	}
+	
+	for (int i = 0; i < results.size(); i++)
+		cout << "Case " << i + 1 << ": " << results[i] << endl;
+	
 	char q;
 	cin >> q;
 	return 0;
