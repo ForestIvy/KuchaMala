23f527446de8d8f4a39514e99905175fa46a9df6
Irina Yatsenko <yveu@hotmail.com>
2017-01-02 12:22:22 -0800

    HOTELS accepted by SPOJ

--- console/console.cpp
+++ console/console.cpp
@@ -10,11 +10,48 @@
 
 using namespace std;
 
+long GetBestOffer(long budget, const vector<long>& hotels)
+{
+	long offer = 0;
+	auto s = hotels.begin();
+	auto e = hotels.begin();
+
+	long cur = 0;
+	while (s != hotels.end())
+	{
+		if (budget - cur < *e)
+		{
+			cur -= *s;
+			++s;
+		}
+		else
+		{
+			cur += *e;
+			if (offer < cur) offer = cur;
+
+			++e;
+			if (e == hotels.end()) return offer;
+		}
+	}
+
+	return offer;
+}
+
 int main()
 {
-	int c;
-	cin >> c;
+	long c, budget;
+	cin >> c >> budget;
 	
+	vector<long> hotels;
+	hotels.reserve(c);
+	for (long i = 0; i < c; i++)
+	{
+		long h;
+		cin >> h;
+		hotels.push_back(h);
+	}
+	cout << GetBestOffer(budget, hotels) << endl;
+
 	char q;
 	cin >> q;
 	return 0;
