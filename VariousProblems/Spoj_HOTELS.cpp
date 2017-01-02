e6f119f04e0cba0f81539e7242ad9bcec7d57577
Irina Yatsenko <yveu@hotmail.com>
2017-01-02 12:16:01 -0800

    Solved for my test cases

--- VariousProblems/Spoj_HOTELS.cpp
+++ VariousProblems/Spoj_HOTELS.cpp
@@ -2,6 +2,7 @@
 
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <limits>
 
 #include <algorithm>
 #include <vector>
@@ -16,12 +17,78 @@ namespace SPOJ
 	{
 		long GetBestOffer(long budget, const vector<long>& hotels)
 		{
-			return 0;
+			long offer = 0;
+			auto s = hotels.begin();
+			auto e = hotels.begin();
+
+			long cur = 0;
+			while (s != hotels.end())
+			{
+				if (budget - cur < *e)
+				{
+					cur -= *s;
+					++s;
+				}
+				else
+				{
+					cur += *e;
+					if (offer < cur) offer = cur;
+
+					++e;
+					if (e == hotels.end()) return offer;
+				}
+			}
+
+			return offer;
 		}
+
+
 	public:
-		TEST_METHOD(SingleCannotBuy)
+		TEST_METHOD(SingleCanBuy_No)
 		{
 			Assert::AreEqual<long>(0, GetBestOffer(3, vector<long>{7}));
+		}
+
+		TEST_METHOD(SingleCanBuy_Yes)
+		{
+			Assert::AreEqual<long>(7, GetBestOffer(8, vector<long>{7}));
+		}
+
+		TEST_METHOD(Prefix)
+		{
+			Assert::AreEqual<long>(9, GetBestOffer(9, vector<long>{5, 4, 2, 1}));
+		}
+
+		TEST_METHOD(Middle)
+		{
+			Assert::AreEqual<long>(9, GetBestOffer(9, vector<long>{1, 5, 4, 2, 1}));
+		}
+
+		TEST_METHOD(Suffix)
+		{
+			Assert::AreEqual<long>(9, GetBestOffer(9, vector<long>{1, 2, 3, 5, 4}));
+		}
+
+		TEST_METHOD(All)
+		{
+			Assert::AreEqual<long>(10, GetBestOffer(10, vector<long>{2, 4, 3, 1}));
+		}
+
+		TEST_METHOD(ExpensivePalace)
+		{
+			Assert::AreEqual<long>(8, GetBestOffer(9, vector<long>{1, 2, 3, 5, 12, 1}));
+		}
+
+		TEST_METHOD(Max)
+		{
+			vector<long> hotels(300000, 1000000);
+			Assert::AreEqual<long>(2147000000, GetBestOffer(numeric_limits<long>::max() - 10, hotels));
+		}
+
+		TEST_METHOD(FromSpoj)
+		{
+			Assert::AreEqual<long>(12, GetBestOffer(12, vector<long>{2, 1, 3, 4, 5}));
+			Assert::AreEqual<long>(8, GetBestOffer(9, vector<long>{7, 3, 5, 6}));
 		}
 	};
 }
