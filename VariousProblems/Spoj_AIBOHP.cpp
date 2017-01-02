1209753b71ed19d6526d50f7f267659ea3952d9c
Irina Yatsenko <yveu@hotmail.com>
2017-01-02 13:47:33 -0800

    Fixed perf in AIBOHP

--- VariousProblems/Spoj_AIBOHP.cpp
+++ VariousProblems/Spoj_AIBOHP.cpp
@@ -12,32 +12,25 @@ using namespace std;
 #if 1
 namespace SPOJ
 {
-	bool IsPalindrome(const string& s)
-	{
-		auto l = s.begin();
-		auto r = s.end() - 1;
-		while (l < r)
-		{
-			if (*l != *r) return false;
-			++l;
-			--r;
-		}
-		return true;
-	}
-
 	int MakePalinCost(const string& s)
 	{
 		if (s.length() < 2) return 0;
 
-		vector<int> costs(s.length(), 0);
+		vector<int> costs_prev(s.length(), 0); 
+		vector<int> costs(s.length() - 1, 0);
+		for (int i = 0; i < costs.size(); i++) costs[i] = (s[i] == s[i + 1]) ? 0 : 1;
+
+		vector<int> cur;
 		while (costs.size() > 1)
 		{
-			for (int i = 0; i < costs.size() - 1; i++)
+			cur.resize(costs.size() - 1);
+			for (int i = 0; i < cur.size(); i++)
 			{
-				if (IsPalindrome(s.substr(i, s.length() - costs.size() + 2))) costs[i] = 0;
-				else costs[i] = 1 + min(costs[i], costs[i + 1]);
-			}
-			costs.resize(costs.size() - 1);
+				if (s[i] == s[i + s.length() - cur.size()]) cur[i] = costs_prev[i+1];
+				else cur[i] = 1 + min(costs[i], costs[i + 1]);
+			}		
+			costs_prev.swap(costs);
+			costs.swap(cur);
 		}
 		return costs.front();
 	}
@@ -45,15 +38,6 @@ namespace SPOJ
 	TEST_CLASS(AIBOHP)
 	{
 	public:
-		TEST_METHOD(IsPalindrome_Test)
-		{
-			Assert::IsTrue(IsPalindrome("a"), L"a");
-			Assert::IsTrue(IsPalindrome("aba"), L"aba");
-			Assert::IsTrue(IsPalindrome("abccba"), L"abccba");
-			Assert::IsFalse(IsPalindrome("ab"), L"ab");
-			Assert::IsFalse(IsPalindrome("aab"), L"aab");
-		}
-
 		TEST_METHOD(SingleLetter)
 		{
 			Assert::AreEqual(0, MakePalinCost("a"));
@@ -71,6 +55,11 @@ namespace SPOJ
 			Assert::AreEqual(0, MakePalinCost("aba"), L"aba");
 			Assert::AreEqual(1, MakePalinCost("abb"), L"abb -> abba");
 			Assert::AreEqual(2, MakePalinCost("abc"), L"abc -> abcba");
+		}
+
+		TEST_METHOD(Middle)
+		{
+			Assert::AreEqual(1, MakePalinCost("abcdacba"), L"abcdabca -> abcdadcba");
 		}
 
 		TEST_METHOD(MaxLength)
