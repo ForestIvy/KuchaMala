2de8f88e920d90f71999a6696541a0569161a71c
Irina Yatsenko <yveu@hotmail.com>
2017-01-08 13:33:40 -0800

    Set up for solving ARRAYSUB problem from SPOJ

---
+++ VariousProblems/Spoj_ARRAYSUB.cpp
@@ -0,0 +1,25 @@
+// http://www.spoj.com/problems/ARRAYSUB/
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <algorithm>
+#include <vector>
+#include <numeric>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+#if 1
+namespace SPOJ
+{
+	TEST_CLASS(ARRAYSUB)
+	{
+	public:
+		TEST_METHOD(SubOfLength1)
+		{
+			Assert::Fail();
+		}
+	};
+}
+#endif
