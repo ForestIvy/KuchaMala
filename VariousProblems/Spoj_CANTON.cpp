f7ea8f7197b157ace48aeb408ff477d07af0bcf5
Irina Yatsenko <yveu@hotmail.com>
2017-01-10 18:53:38 -0800

    Set up for solving CANTON from Spoj

---
+++ VariousProblems/Spoj_CANTON.cpp
@@ -0,0 +1,20 @@
+// http://www.spoj.com/problems/CANTON
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <algorithm>
+#include <vector>
+#include <numeric>
+
+#include "ToString.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+#if 1
+namespace SPOJ
+{
+
+}
+#endif
